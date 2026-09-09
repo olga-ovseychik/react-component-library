b38eda6e343c33647ca9f4b5686e3cc905176985
Volha Marozava <olga.ovsejchik@gmail.com>
2026-09-09 12:57:13 +0200

    feat: export ToastContainer

---
+++ src/components/Toast/index.ts
@@ -0,0 +1,2 @@
+export { ToastContainer } from './ToastContainer'
+export * from "./types";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./Tabs";
 export * from './Accordion'
 export * from './Toggle'
+export * from './Toast'
